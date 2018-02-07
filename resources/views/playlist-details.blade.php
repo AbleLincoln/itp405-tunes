13c9818aeb9d7f798eaf416ed9f33e616c3d74a7
Andrew Maney <amaney@usc.edu>
2018-02-06 18:44:11 -0800

    templating with main-layout

--- resources/views/playlist-details.blade.php
+++ resources/views/playlist-details.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Playlist Details</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-</head>
-<body>
+@extends('main-layout')
+
+@section('title', $playlist->Name)
+
+@section('content')
   <h1>{{$playlist->Name}}</h1>
 
   <h3>Tracks</h3>
@@ -18,6 +13,4 @@
       </li>
     @endforeach
   </ul>
-  
-</body>
-</html>
+@endsection
--- resources/views/playlist-list.blade.php
+++ resources/views/playlist-list.blade.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Playlists</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-  <script src="main.js"></script>
-</head>
-<body>
+@extends('main-layout')
 
-<ul>
-  @foreach($playlists as $playlist)
-  <li>
-    <a href='/playlists/{{$playlist->PlaylistId}}'>{{$playlist->Name}}</a>
-  </li>
-  @endforeach
-</ul>
-  
-</body>
-</html>
+@section('title')
+  Playlists
+@endsection
+
+@section('content')
+  <ul>
+    @foreach($playlists as $playlist)
+    <li>
+      <a href='/playlists/{{$playlist->PlaylistId}}'>{{$playlist->Name}}</a>
+    </li>
+    @endforeach
+  </ul>
+@endsection
